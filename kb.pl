48a1c6398eaf80817522474afacedade36c3b451
emilyjia <emilyjia2010@gmail.com>
2021-04-06 00:05:26 -0700

    url construct works now

--- kb.pl
+++ kb.pl
@@ -4,7 +4,7 @@ query("vegetarian", "&health=vegetarian").
 query("no pork", "&health=pork-free").
 query("pescatarian", "&health=pescatarian").
 query("no peanuts", "&health=peanut-free").
-query("no diary", &health=dairy-free").
+query("no diary", "&health=dairy-free").
 query("paleo", "&health=paleo").
 query("kosher", "&health=kosher").
 query("no soy", "&health=soy-free").
@@ -14,14 +14,14 @@ query("no shellfish", "&health=shellfish-free").
 query("low sugar", "&health=sugar-conscious").
 
 query("high fiber", "&diet=high-fiber").
-query("high protein", "diet=high-protein").
+query("high protein", "&diet=high-protein").
 query("low carb", "&diet=low-carb").
-query("low fat", &diet=low-fat).
-query("low sodium", &diet=low-sodium).
+query("low fat", "&diet=low-fat").
+query("low sodium", "&diet=low-sodium").
 
 query("breakfast", "&mealType=Breakfast").
 query("lunch", "&mealType=Lunch").
-query("dinner", &mealType=Dinner).
+query("dinner", "&mealType=Dinner").
 query("snack", "&mealType=Snack").
 query("dessert", "&dishType=Desserts").
 query("drink", "&dishType=Drinks").
